9891945829d2ecbfa43967eba3dc20bd6e40cfb1
Jordi <jordi.estrada.jordi@gmail.com>
2025-11-10 17:53:46 +0100

    ADD, map from fake databasse added WITH BUGS

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
+"use client";
+
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [tasks, setTasks] = useState([]);
+
+async function getTasks() {
+  try {
+    const res = await fetch("http://localhost:3000/database.json");
+    if (!res.ok) throw new Error("Failed to fetch tasks");
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return [];
+  }
+}
+
+async function setTasksData() {
+  const tasksData = await getTasks();
+  setTasks(tasksData);
+}
+
+setTasksData();
+
+
+  
+  console.log("dev ~ Home ~ tasks:", tasks)
   return (
-    <main>
+    <main style={{ margin: 20 }}>
       <h1>Welcome to My Next.js App</h1>
-      <div className="customCard">
-        <div>
-          <h1>Passar la fregona</h1>
-          <p>Score: 10</p>
+      {/* {tasks.map((task, index) => (
+        <div key={index} className="customCard">
+          <div>
+            <h1>{task.name}</h1>
+            <p>Score: {task.score}</p>
+          </div>
         </div>
-      </div>
-    </main> 
+      ))} */}
+      {/* Hardcoded example card */}
+      
+    </main>
   );
 }
