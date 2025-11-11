0e1b663ccbc722dacdc642d5e24dea8a1ee7d51b
Jordi <jordi.estrada.jordi@gmail.com>
2025-11-11 17:41:49 +0100

    REFACTOR, Client and server separated in page.tsx

---
+++ app/components/PendingTasksList.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+type PendingTasksListProps = {
+  tasks: object[];
+};
+
+export default function PendingTasksList({tasks}: PendingTasksListProps) {
+console.log("dev ~ PendingTasksList ~ tasks:", tasks)
+
+    
+  return (
+    <main style={{ margin: 20 }}>
+      <h1>Welcome to My Next.js App</h1>
+      {tasks.map((task, index) => (
+        <div key={index} className="customCard">
+          <div>
+            <h1>{task.name}</h1>
+            <p>Score: {task.score}</p>
+          </div>
+        </div>
+      ))}
+      {/* Hardcoded example card */}
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,17 @@
-"use client";
-
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import PendingTasksList from "./components/PendingTasksList";
+import { get } from "http";
 
-export default function Home() {
-  const [tasks, setTasks] = useState([]);
+export default async function Home() {
 
-async function getTasks() {
-  try {
     const res = await fetch("http://localhost:3000/database.json");
-    if (!res.ok) throw new Error("Failed to fetch tasks");
-    return await res.json();
-  } catch (error) {
-    console.error("Error fetching tasks:", error);
-    return [];
-  }
-}
-
-async function setTasksData() {
-  const tasksData = await getTasks();
-  setTasks(tasksData);
-}
-
-setTasksData();
+    const tasks = await res.json();
+    console.log("dev ~ Home ~ tasks:", tasks)
 
 
-  
-  console.log("dev ~ Home ~ tasks:", tasks)
   return (
     <main style={{ margin: 20 }}>
-      <h1>Welcome to My Next.js App</h1>
-      {/* {tasks.map((task, index) => (
-        <div key={index} className="customCard">
-          <div>
-            <h1>{task.name}</h1>
-            <p>Score: {task.score}</p>
-          </div>
-        </div>
-      ))} */}
-      {/* Hardcoded example card */}
-      
+      <PendingTasksList tasks={tasks} />
     </main>
   );
 }
