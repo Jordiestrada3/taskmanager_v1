aef5718b8fd84a3d04631fe6cc7f7a470652647c
Jordi <jordi.estrada.jordi@gmail.com>
2025-11-13 17:08:22 +0100

    ADD, footer and header added to pages layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,20 @@ import PendingTasksList from "./components/PendingTasksList";
 import { get } from "http";
 import { getTasks, createTask } from "../utils/utils";
 import TaskForm from "./components/TaskForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 export default async function Home() {
-
   const tasks = await getTasks();
 
   return (
-    <main style={{ margin: 20 }}>
-      <PendingTasksList tasks={tasks} />
-      <TaskForm />
-    </main>
+    <div className="site">
+      <Header />
+      <main>
+        <PendingTasksList tasks={tasks} />
+        <TaskForm />
+      </main>
+      <Footer />
+    </div>
   );
 }
