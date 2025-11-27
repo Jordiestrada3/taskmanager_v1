a83e8bd77ee599e8353b97c615cb435256bb19f5
Jordi <jordi.estrada.jordi@gmail.com>
2025-11-27 16:41:28 +0100

    ADD, Delete button added

---
+++ app/components/DeleteButon.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+type DeleteButtonProps = {
+  tasks: object[];
+};
+
+export default function DeleteButton({ tasks }: DeleteButtonProps) {
+
+
+  return (
+   
+  );
+}
