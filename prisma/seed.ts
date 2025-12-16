e43f70adb646be1e5bcba15fd3051b2a7fee7b2e
Jordi <jordi.estrada.jordi@gmail.com>
2025-12-16 19:10:58 +0100

    REFACTOR, id from prisma schemma changed to uuid

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,6 +15,10 @@ const userData: Prisma.UserCreateInput[] = [
     name: "John Cena",
     score: 0,
   },
+  {
+    name: "Jane Doe",
+    score: 0,
+  },
 ];
 
 const taskData: Prisma.TaskCreateInput[] = [
@@ -24,6 +28,13 @@ const taskData: Prisma.TaskCreateInput[] = [
     description: "Netejar tota la casa amb escombra i recollir la brutícia.",
     frequencyTime: 86400000, // in ms (24 hours)
     lastTimeDone: Date.now() - 259200000, // timestamp 3 days ago from now
+  },
+  {
+    name: "Fer el menjar",
+    score: 5,
+    description: "Preparar el menjar per a tota la família.",
+    frequencyTime: 172800000, // in ms (48 hours)
+    lastTimeDone: Date.now() - 172800000, // timestamp 48 hours ago from now
   },
 ];
 
