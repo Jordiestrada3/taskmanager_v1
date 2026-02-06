4b3b75ee7e32062bfcbfb69c3d8611083f518127
Jordi <jordi.estrada.jordi@gmail.com>
2026-02-06 17:19:58 +0100

    ADD, Loader added

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,6 +4,7 @@ import PendingTasksList from "./components/PendingTasksList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import prisma from "@/lib/prisma";
+import { Loader } from "lucide-react";
 
 export const dynamic = "force-dynamic"; // To show latest data in the build
 
@@ -15,8 +16,8 @@ export default async function Loading() {
   return (
     <div className="site">
       <Header />
-      <main style={{ margin: 10, color: 'grey' }}>
-        Loading...
+      <main style={{ margin: 10, color: 'grey', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <span className="loader"/>
       </main>
       <Footer />
     </div>
